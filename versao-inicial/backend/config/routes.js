e9f740e5fcb3c8cddbfa98d8ecd87bb45cda5e30
Loui Satto <x_sato@hotmail.com>
2019-12-27 16:34:37 +0900

    ate a aula de article cod3r knowledge

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -1,4 +1,32 @@
 module.exports = app => {
-    app.route('./users')
-    .get(app.api.user.save)
+    app.route('/users')
+        .post(app.api.user.save)
+        .get(app.api.user.get)
+
+    app.route('/users/:id')
+        .put(app.api.user.save)
+        .get(app.api.user.getById)
+
+    app.route('/categories')
+        .post(app.api.category.save)
+        .get(app.api.category.get)
+
+    // cuidado com a ordem! Tem que vir antes de /categories:id, senao ele acha que tree e o id
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
+    app.route('/categories/:id')
+        .put(app.api.category.save)
+        .get(app.api.category.getById)
+        .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
 }
