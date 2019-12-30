7653908dc7bdbeece0b933a49ea8a21f68eab7cd
Loui Satto <x_sato@hotmail.com>
2019-12-30 23:44:18 +0900

    ate o passport

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -5,36 +5,44 @@ module.exports = app => {
     app.post('/validatToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
     app.route('/categories')
+        .all(app.config.passport.authenticate())
         .post(app.api.category.save)
         .get(app.api.category.get)
 
     // cuidado com a ordem! Tem que vir antes de /categories:id, senao ele acha que tree e o id
 
     app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.category.save)
         .get(app.api.category.getById)
         .delete(app.api.category.remove)
 
     app.route('/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.get)
         .post(app.api.article.save)
 
     app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
         .put(app.api.article.save)
         .delete(app.api.article.remove)
 
     app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 }
