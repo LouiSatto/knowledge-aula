b06bcf7065dc854aa0b6f7c6678d124c9f4885e8
Loui Satto <x_sato@hotmail.com>
2020-01-08 22:38:25 +0900

    vue toasted msg ok

--- versao-inicial/frontend/src/config/msgs.js
+++ versao-inicial/frontend/src/config/msgs.js
@@ -1,19 +1,19 @@
-// import Vue from 'vue'
-// import Toasted from 'vue-toasted'
+import Vue from 'vue'
+import Toasted from 'vue-toasted'
 
-// Vue.use(Toasted, {
-//     iconPack: 'fontawesome',
-//     duration: 3000
-// })
+Vue.use(Toasted, {
+    iconPack: 'fontawesome',
+    duration: 3000
+})
 
-// Vue.Toasted.register(
-//     'defaultSuccess',
-//     payload => !payload.msg ? 'Operacao realizada com sucesso!' : payload.msg,
-//     { type: 'success', icon: 'check' }
-// )
+Vue.toasted.register(
+    'defaultSuccess',
+    payload => !payload.msg ? 'Operacao realizada com sucesso!' : payload.msg,
+    { type: 'success', icon: 'check' }
+)
 
-// Vue.Toasted.register(
-//     'defaultError',
-//     payload => !payload.msg ? 'Oops.. Erro inesperado.' : payload.msg,
-//     { type: 'error', icon: 'times' }
-// )
+Vue.toasted.register(
+    'defaultError',
+    payload => !payload.msg ? 'Oops.. Erro inesperado.' : payload.msg,
+    { type: 'error', icon: 'times' }
+)
