2d011b3c03804d7be43c9e9f5d8dffbb8809f796
Loui Satto <x_sato@hotmail.com>
2020-01-08 22:15:21 +0900

    userAdmin ok msg toasted ng

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
 
 // TEMPORARIO!!!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwibmFtZSI6ImNhcmxvcyB0cmVzIiwiZW1haWwiOiJjYXJsb3N0cmVzQGhvdG1haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE1NzgzMTgxODcsImV4cCI6MTU3ODU3NzM4N30.mcTc06qVJijIFgRJqBp2gi_2qVU-cRp1bF2baZGs8E8'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikx1aXMgQ2FybG9zIFNhdG8iLCJlbWFpbCI6Inhfc2F0b0Bob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1Nzg0MDY1OTAsImV4cCI6MTU3ODY2NTc5MH0.CJgPmvysjb8-oz4cgG94NDOL_Yr5jInsjR5-0Auq608'
 
 new Vue({
   store,
